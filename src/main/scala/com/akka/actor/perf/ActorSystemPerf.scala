c8d474fe751cbf03fd42f23d106806341bf7c317
Upadhyay <prayag.upd@gmail.com>
2018-03-24 00:54:06 -0700

    refactor namespaces

--- src/main/scala/com/akka/actor/perf/ActorSystemPerf.scala
+++ src/main/scala/com/akka/actor/perf/ActorSystemPerf.scala
@@ -22,29 +22,3 @@ object ActorSystemPerf {
   }
 
 }
-
-class IntentFinder extends Actor with ActorLogging {
-
-  import IntentFinder._
-
-  override def receive: Receive = {
-
-    case FindIntent(utterance: String) =>
-      println(s"received $utterance")
-
-      println("responding intent")
-      IntentNotification("cool-intent-name")
-
-    case _ => InvalidIntentEvent("send me good utterances")
-  }
-}
-
-object IntentFinder {
-
-  case class FindIntent(utterance: String)
-
-  case class IntentNotification(intentName: String)
-
-  case class InvalidIntentEvent(info: String)
-
-}
